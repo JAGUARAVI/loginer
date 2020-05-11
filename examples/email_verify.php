b015bce6e6174e803fa344316c14a17c4a844c77
JAGUARAVI <47686625+JAGUARAVI@users.noreply.github.com>
2020-05-11 08:11:08 +0530

    Update email_verify.php

--- examples/email_verify.php
+++ examples/email_verify.php
@@ -1,17 +1,23 @@
 <?php
 //require 'path/to/loginer.main.php';
 require '../loginer.main.php';
+
 $user = new Loginer();
-$user->emailVerify($_REQUEST['username'],$_REQUEST['code']);
-if($user == true){
-    header('Refresh: 10;URL=../');
-    echo "Verification Successful !<br>Redirecting in 10 Seconds...";
-}
-else{
-    $errors = $user->get_errors();
-    foreach ($errors as $error){
-        echo $error.'<br>';
+
+if(isset($_REQUEST['username']) && isset($_REQUEST['code'])){
+    $user->emailVerify($_REQUEST['username'],$_REQUEST['code']);
+    if($user == true){
+        header('Refresh: 10;URL=../');
+        echo "Verification Successful !<br>Redirecting in 10 Seconds...";
+    }
+    else{
+        $errors = $user->get_errors();
+        foreach ($errors as $error){
+            echo $error.'<br>';
+        }
     }
 }
-
+else{
+    echo "Please enter username and verification code for email verification";
+}
 ?>
