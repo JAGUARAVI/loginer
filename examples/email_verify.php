022ca4f782cc60ea2e7e4aedd090d3e139d0844a
JAGUARAVI <47686625+JAGUARAVI@users.noreply.github.com>
2020-05-11 07:55:53 +0530

    Update email_verify.php

--- examples/email_verify.php
+++ examples/email_verify.php
@@ -1,6 +1,6 @@
 <?php
 //require 'path/to/loginer.main.php';
-require 'loginer.main.php';
+require '../loginer.main.php';
 $user = new Loginer();
 $user->emailVerify($_REQUEST['username'],$_REQUEST['code']);
 if($user == true){
