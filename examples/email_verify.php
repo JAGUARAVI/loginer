603125471318e5f1391fbf0d5ad23bc11e048d7f
JAGUARAVI <47686625+JAGUARAVI@users.noreply.github.com>
2020-05-11 07:55:37 +0530

    Create email_verify.php

---
+++ examples/email_verify.php
@@ -0,0 +1,17 @@
+<?php
+//require 'path/to/loginer.main.php';
+require 'loginer.main.php';
+$user = new Loginer();
+$user->emailVerify($_REQUEST['username'],$_REQUEST['code']);
+if($user == true){
+    header('Refresh: 10;URL=../');
+    echo "Verification Successful !<br>Redirecting in 10 Seconds...";
+}
+else{
+    $errors = $user->get_errors();
+    foreach ($errors as $error){
+        echo $error.'<br>';
+    }
+}
+
+?>
